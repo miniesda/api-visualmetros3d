bc87af76ecb1479db47969399bcbe01fd95e5810
miniesda <miniesda@gmail.com>
2026-03-11 16:08:37 +0100

    Update [id].js

--- api/metroSevilla/[id].js
+++ api/metroSevilla/[id].js
@@ -2,9 +2,6 @@ import axios from 'axios';
 import https from 'https';
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
