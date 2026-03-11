194cbe0c92844dbbb2ac99c3254f2e248608a4b2
David Rodriguez Rives   -   The Pirate Cat <63803821+miniesda@users.noreply.github.com>
2026-03-11 13:33:13 +0100

    Improve error handling in metroSevilla API
    Enhanced error handling by adding the error stack to the response.

--- api/metroSevilla/[id].js
+++ api/metroSevilla/[id].js
@@ -36,10 +36,12 @@ export default async function handler(req, res) {
     return res.status(200).json(data);
 
   } catch (error) {
-    console.error(error);
+    console.error("ERROR:", error);
+
     return res.status(500).json({
       error: "Proxy error",
-      message: error.message
+      message: error.message,
+      stack: error.stack
     });
   }
 }
